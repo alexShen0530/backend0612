a12c0ef1bb3bf323ae660bb46021c485d37ac37e
Alex_shen <alex.shen@sandalwoodadvisors.com>
2026-06-12 17:38:57 +0800

    2nd push

--- app/schemas/Users.py
+++ app/schemas/Users.py
@@ -5,5 +5,3 @@ class Users(BaseModel):
     username: str = Field(..., min_length=3, max_length=10, description="用户名")
     password: str
     email: str = Field(default='', description="邮箱")
-
-    爱你
