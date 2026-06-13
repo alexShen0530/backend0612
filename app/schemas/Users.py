bcea3467a1c9aeab9fa8dc2dcc52c1329bd74310
alexis.shen <sx1524267334@outlook.com>
2026-06-13 09:48:21 +0800

    test

--- app/schemas/Users.py
+++ app/schemas/Users.py
@@ -5,3 +5,4 @@ class Users(BaseModel):
     username: str = Field(..., min_length=3, max_length=10, description="用户名")
     password: str
     email: str = Field(default='', description="邮箱")
+    信号
